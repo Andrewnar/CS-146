85e87e351c5eef153e0eb0c116a979df2b458323
MonthlyPeep <90810080+MonthlyPeep@users.noreply.github.com>
2021-12-04 12:54:23 -0500

    Bare bones information adder working

--- templates/Classic Template/classic_JS.js
+++ templates/Classic Template/classic_JS.js
@@ -2,3 +2,26 @@ var loadFile = function(event) {
 		var image = document.getElementById('output');
 		image.src = URL.createObjectURL(event.target.files[0]);
 	};
+
+function concactInfo() {  
+	var mylist = document.getElementById("contactInfo");  
+	document.getElementById("contactInfo").value = mylist.options[mylist.selectedIndex].text;  
+	}  
+
+function updateInfo() {
+	var infoType = document.getElementById("contactInfo").value;
+	var info = document.getElementById("information").value;
+
+	if (infoType == "LinkedIn") {
+		var linkedin = document.getElementById('linkedin');
+		linkedin.insertAdjacentHTML('afterend', "     ".concat(info));
+	}
+	if (infoType == "GitHub") {
+		var github = document.getElementById('github');
+		github.insertAdjacentHTML('afterend', "     ".concat(info));
+	}
+	if (infoType == "Email") {
+		var email = document.getElementById('email');
+		email.insertAdjacentHTML('afterend', "     ".concat(info));
+	}
+}
