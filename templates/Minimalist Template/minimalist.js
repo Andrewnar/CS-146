ab4f866414b8901c02c9af45e69b7d650efbde9c
Andrew Narvaez <anarvaez12@verizon.net>
2021-12-05 11:22:57 -0500

    Minimalist JS
    Not working

--- templates/Minimalist Template/minimalist.js
+++ templates/Minimalist Template/minimalist.js
@@ -5,3 +5,25 @@ function colorSelect(element, code) {
     for(var i = 0; i < elms.length; i++) 
         elms[i].style.backgroundColor = element.value; 
 }
+function concactInfo() {  
+	var mylist = document.getElementById("contactInfo");  
+	document.getElementById("contactInfo").value = mylist.options[mylist.selectedIndex].text;  
+}  
+
+function updateInfo() {
+	var infoType = document.getElementById("contactInfo").value;
+	var info = document.getElementById("information").value;
+
+	if (infoType == "LinkedIn") {
+		var linkedin = document.getElementById('linkedInLink');
+		linkedin.setAttribute('href', info);
+	}
+	if (infoType == "GitHub") {
+		var github = document.getElementById('githubLink');
+		github.setAttribute('href', info);
+	}
+	if (infoType == "Email") {
+		var email = document.getElementById('emailLink');
+		email.setAttribute('href', info);
+	}
+}
