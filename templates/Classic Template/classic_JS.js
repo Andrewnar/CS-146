db07197aa15c690c4cbedd3594cf9c38cd1ca878
Chry0nic <68797439+Chry0nic@users.noreply.github.com>
2021-12-06 17:41:46 -0500

    Update classic_JS.js

--- templates/Classic Template/classic_JS.js
+++ templates/Classic Template/classic_JS.js
@@ -49,3 +49,8 @@ function updateInfo() {
 	document.getElementById("information").value = " ";
 
 }
+function changeColor() {
+	let color = document.getElementById('colorInputColor').value;
+	document.body.style.backgroundColor = color;
+	document.getElementById('colorInputText').value = color;
+}
