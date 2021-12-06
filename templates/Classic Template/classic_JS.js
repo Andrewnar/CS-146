7b7535d0943d2ded24e69d710c19ec91f6a4c66d
Andrew Narvaez <anarvaez12@verizon.net>
2021-12-06 12:40:08 -0500

    Classic Update
    Inserted new selection to be used to fill out forms...

--- templates/Classic Template/classic_JS.js
+++ templates/Classic Template/classic_JS.js
@@ -6,7 +6,29 @@ var loadFile = function(event) {
 function concactInfo() {  
 	var mylist = document.getElementById("contactInfo");  
 	document.getElementById("contactInfo").value = mylist.options[mylist.selectedIndex].text;  
-	}  
+}  
+
+function contentInfo() {  
+	var mylist = document.getElementById("contentInfo");  
+	document.getElementById("contentInfo").value = mylist.options[mylist.selectedIndex].text;  
+}
+
+function updateContent(){
+	var infoType = document.getElementById("contentInfo").value;
+	var info = document.getElementById("userContent").value;
+
+	if (infoType == "About Me") {
+		document.getElementById("about").innerHTML = info;
+	} else if (infoType == "Academics"){
+		document.getElementById("academic").innerHTML = info;
+	} else if (infoType == "Experience"){
+		document.getElementById("exp").innerHTML = info;
+	} else if (infoType == "Achievements"){
+		document.getElementById("achievements").innerHTML = info;
+	}
+
+	document.getElementById("userContent").value = " ";
+}
 
 function updateInfo() {
 	var infoType = document.getElementById("contactInfo").value;
