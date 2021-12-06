02e334c4d81387e3c32d17c38b8c921c94d80958
Chry0nic <68797439+Chry0nic@users.noreply.github.com>
2021-12-06 17:43:15 -0500

    Update modern_JS.js

--- templates/Modern Template/modern_JS.js
+++ templates/Modern Template/modern_JS.js
@@ -22,3 +22,8 @@ function updateContent(){
 
 	document.getElementById("userContent").value = " ";
 }
+function changeColor() {
+	let color = document.getElementById('colorInputColor').value;
+	document.body.style.backgroundColor = color;
+	document.getElementById('colorInputText').value = color;
+}
