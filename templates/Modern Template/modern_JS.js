463aaa117269f9ecfb6914be506dae0fd6dc0b41
Andrew Narvaez <anarvaez12@verizon.net>
2021-12-06 12:51:02 -0500

    Modern About Forms
    Inserted forms in modern about

--- templates/Modern Template/modern_JS.js
+++ templates/Modern Template/modern_JS.js
@@ -1,4 +1,24 @@
 var loadFile = function(event) {
 		var image = document.getElementById('output');
 		image.src = URL.createObjectURL(event.target.files[0]);
-	};
+}
+
+function contentInfo() {  
+	var mylist = document.getElementById("contentInfo");  
+	document.getElementById("contentInfo").value = mylist.options[mylist.selectedIndex].text;  
+}
+
+function updateContent(){
+	var infoType = document.getElementById("contentInfo").value;
+	var info = document.getElementById("userContent").value;
+
+	if (infoType == "Background") {
+		document.getElementById("background").innerHTML = info;
+	} else if (infoType == "Interests"){
+		document.getElementById("interests").innerHTML = info;
+	} else if (infoType == "Hobbies"){
+		document.getElementById("hobbies").innerHTML = info;
+	}
+
+	document.getElementById("userContent").value = " ";
+}
